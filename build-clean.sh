b822d881d4a97872d73f95fffdf5824d80957b91
Ronald Huizer <r.huizer@xs4all.nl>
2013-04-26 13:47:55 -0400

    Zapped autotools in favor of CMake.
    Signed-off-by: Ronald Huizer <r.huizer@xs4all.nl>

--- build-clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf autom4te.cache/ src/.deps examples/.deps
-rm -f aclocal.m4 autoscan.log config.guess config.sub configure
-rm -f depcomp install-sh ltmain.sh Makefile.in missing stamp-h1
-rm -f config.h config.h.in config.h.in~ config.status libtool
-rm -f src/Makefile.in src/Makefile Makefile config.log examples/Makefile
-rm -f examples/Makefile.in
